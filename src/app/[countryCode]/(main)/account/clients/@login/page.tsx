143243059d50ef175eb2e4e79e184514d2c90565
Michael Wick <mike2036@hotmail.com>
2026-03-18 07:48:42 -0600

    initial

---
+++ src/app/[countryCode]/(main)/account/clients/@login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next"
+
+import LoginTemplate from "@modules/account/templates/login-template"
+
+export const metadata: Metadata = {
+  title: "Sign in",
+  description: "Sign in to your Medusa Store account.",
+}
+
+export default function Login() {
+  return 'Hi'
+  return <LoginTemplate memberType="client" />
+}
---
+++ src/modules/account/components/register/index.tsx
@@ -0,0 +1,114 @@
+"use client"
+
+import { useActionState } from "react"
+import Input from "@modules/common/components/input"
+import { LOGIN_VIEW } from "@modules/account/templates/login-template"
+import ErrorMessage from "@modules/checkout/components/error-message"
+import { SubmitButton } from "@modules/checkout/components/submit-button"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import { signup } from "@lib/data/customer"
+
+type Props = {
+  memberType: "client" | "developer"
+  setCurrentView: (view: LOGIN_VIEW) => void
+}
+
+const Register = ({ memberType, setCurrentView }: Props) => {
+  const [message, formAction] = useActionState(signup, null)
+
+  const registerMessage = memberType === "client"
+    ? "Create your BugZapper Client profile, and start posting bugs."
+    : "Create your BugZapper Developer profile, and earn money zapping bugs."
+
+  return (
+    <div
+      className="max-w-sm flex flex-col items-center"
+      data-testid="register-page"
+    >
+      <h1 className="text-large-semi uppercase mb-6">
+        Become a BugZapper Member!
+      </h1>
+      <p className="text-center text-base-regular text-ui-fg-base mb-4">
+        {registerMessage}
+      </p>
+      <form className="w-full flex flex-col" action={formAction}>
+        <div className="flex flex-col w-full gap-y-2">
+          <Input
+            label="First name"
+            name="first_name"
+            required
+            autoComplete="given-name"
+            data-testid="first-name-input"
+          />
+          <Input
+            label="Last name"
+            name="last_name"
+            required
+            autoComplete="family-name"
+            data-testid="last-name-input"
+          />
+          <Input
+            label="Email"
+            name="email"
+            required
+            type="email"
+            autoComplete="email"
+            data-testid="email-input"
+          />
+          {
+            memberType === "client" && (
+              <Input
+                label="Company name"
+                name="company"
+                required
+                autoComplete="organization"
+                data-testid="company-input"
+              />
+            )
+          }
+          <Input
+            label="Password"
+            name="password"
+            required
+            type="password"
+            autoComplete="new-password"
+            data-testid="password-input"
+          />
+        </div>
+        <ErrorMessage error={message} data-testid="register-error" />
+        <span className="text-center text-ui-fg-base text-small-regular mt-6">
+          By creating an account, you agree to Medusa Store&apos;s{" "}
+          <LocalizedClientLink
+            href="/content/privacy-policy"
+            className="underline"
+          >
+            Privacy Policy
+          </LocalizedClientLink>{" "}
+          and{" "}
+          <LocalizedClientLink
+            href="/content/terms-of-use"
+            className="underline"
+          >
+            Terms of Use
+          </LocalizedClientLink>
+          .
+        </span>
+        <SubmitButton className="w-full mt-6" data-testid="register-button">
+          Join
+        </SubmitButton>
+      </form>
+      <span className="text-center text-ui-fg-base text-small-regular mt-6">
+        Already a member?{" "}
+        <button
+          onClick={() => setCurrentView(LOGIN_VIEW.SIGN_IN)}
+          className="underline"
+        >
+          Sign in
+        </button>
+        .
+      </span>
+    </div>
+  )
+}
+
+export default Register
