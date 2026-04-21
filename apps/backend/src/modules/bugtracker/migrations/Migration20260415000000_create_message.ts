d4d16d034c49870a007769b3a9526c5101182271
Michael Wick <mike2036@hotmail.com>
2026-04-21 11:51:57 -0600

    more updated scripts

--- apps/backend/src/modules/bugtracker/migrations/Migration20260415000000_create_message.ts
+++ apps/backend/src/modules/bugtracker/migrations/Migration20260415000000_create_message.ts
@@ -1,10 +1,11 @@
+// src/modules/bugtracker/migrations/Migration20260415000000_create_message.ts
 import { Migration } from "@medusajs/framework/mikro-orm/migrations"
 
-export class Migration202601415000001 extends Migration {
+export class Migration20260415000000 extends Migration {
   override async up(): Promise<void> {
     this.addSql(`create table if not exists "message" (
       "id" text not null,
-      "submission_id" text not null,
+      "submission_id" text null,
       "created_at" timestamptz not null default now(),
       "updated_at" timestamptz not null default now(),
       "deleted_at" timestamptz null,
